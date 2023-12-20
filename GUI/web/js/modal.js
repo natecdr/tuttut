2161b34aea28fd21b19633f122edc830f5a25c6b
natecdr <nathan.candre@gmail.com>
2023-12-20 11:08:53 +0100

    Add unlinked inputs in modal

--- GUI/web/js/modal.js
+++ GUI/web/js/modal.js
@@ -12,25 +12,6 @@
 *   - Promise<string>
  */
 const displayModal = (title, description, buttonOptions=['Yes', 'No'], closeEvent='Close') => {
-    const buildHeader = (_title) => {
-      const header = document.createElement('div');
-      header.classList.add('modal-section', 'modal-header');
-
-      const closeButton = document.createElement('span');
-      closeButton.classList.add('close-btn');
-      closeButton.innerHTML = '&times;';
-      header.appendChild(closeButton);
-
-      const titleElement = document.createElement('h2');
-      titleElement.innerText = _title;
-      header.appendChild(titleElement);
-
-      return {
-          header: header,
-          closeButton: closeButton
-      }
-    };
-
     const buildBody = (_description) => {
         const modalBody = document.createElement('div');
         modalBody.classList.add('modal-section');
@@ -66,40 +47,28 @@ const displayModal = (title, description, buttonOptions=['Yes', 'No'], closeEven
     const modalArea = document.getElementById("modal-area");
     modalArea.classList.remove('modal-coverage-hidden');
 
-    const headerElement = buildHeader(title);
-    const bodyElement = buildBody(description);
-    const footerElement = buildFooter();
-    const footerButtons = footerElement.footerButtons;
+    closeButton = document.getElementById("close-btn")
+
 
     const clearEventListeners = () => {
-        headerElement.closeButton.removeEventListener('click', (_) => {});
-        footerButtons.forEach((button) => button.removeEventListener('click', (_) => {}));
+        closeButton.removeEventListener('click', (_) => {});
+        // footerButtons.forEach((button) => button.removeEventListener('click', (_) => {}));
     };
 
-    const modalContent = document.createElement('div');
-    modalContent.classList.add('modal-content');
-
-    modalContent.appendChild(headerElement.header);
-    modalContent.appendChild(bodyElement.body);
-    modalContent.appendChild(footerElement.footer);
-
-    modalArea.appendChild(modalContent);
-
     return new Promise((resolve) => {
-        headerElement.closeButton.addEventListener('click', (_) => {
+        closeButton.addEventListener('click', (_) => {
             clearEventListeners();
-            modalArea.removeChild(modalContent);
             modalArea.classList.add('modal-coverage-hidden');
             resolve(closeEvent);
         });
 
-        for (const [label, button] of zip(buttonOptions, footerButtons)) {
-            button.addEventListener('click', (_) => {
-                clearEventListeners();
-                modalArea.removeChild(modalContent);
-                modalArea.classList.add('modal-coverage-hidden');
-                resolve(label);
-            });
-        }
+        // for (const [label, button] of zip(buttonOptions, footerButtons)) {
+        //     button.addEventListener('click', (_) => {
+        //         clearEventListeners();
+        //         modalArea.removeChild(modalContent);
+        //         modalArea.classList.add('modal-coverage-hidden');
+        //         resolve(label);
+        //     });
+        // }
     })
 };
